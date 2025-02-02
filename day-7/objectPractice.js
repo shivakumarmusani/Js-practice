a6b2064bd97c55037a20d2236bcdf459c275a41b
shivakumarmusani <shivakumarmusani@gmail.com>
2025-02-02 21:28:03 +0530

    add

---
+++ day-7/objectPractice.js
@@ -0,0 +1,15 @@
+const move1 = {
+    name: "avatar",
+    price: 200,
+    language: "english"
+};
+
+// const movie2 = {};
+// movie2.name = "batman";
+// moviee.price = 180;
+// movie2.language = "hindi";
+
+// const move3 = {};
+// movie3["name"] = "superman";
+// movie3["price"] = 220;
+// movie3["language"] = "telugu";
