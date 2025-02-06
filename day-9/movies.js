6ed1f4bb1335e5d51f8f8857566034ed36e80d62
shivakumarmusani <138885221+shivakumarmusani@users.noreply.github.com>
2025-02-06 18:26:41 +0530

    add

--- day-9/movies.js
+++ day-9/movies.js
@@ -1,20 +1,16 @@
-//Day 9 Assignment: Basic Movie Ticket
-//System
-// //Assignment 1: Create Movie Objects (Like Filling a Form)
-// Task:
-// 1. Create a movie object with:
-// • Title
-// • Time
-// • Seats available
-
 //movies.js
 const movie1 = {
-    titile: "Kalki",
-    time: "10AM",
+    title: "kalki",
+    time:"10AM",
     seats: 60,
 };
 const movie2 = {
-    titile: "Pushpa 2",
-    time: "3PM",
-    seats: 60,
+    title: "Pushpa 2",
+    time: "2PM",
+    seats: 80,
+};
+const movie3 = {
+    title: "KGF",
+    time: "6PM",
+    seats: 90,
 };
