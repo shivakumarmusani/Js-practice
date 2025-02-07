f42fed3964a1b1baeb8c1e10aeb0102fc1e46f35
shivakumarmusani <138885221+shivakumarmusani@users.noreply.github.com>
2025-02-07 19:40:13 +0530

    add

---
+++ components/data.js
@@ -0,0 +1,31 @@
+export const movie =[{
+    id:1,
+    name: "shiva",
+    age: 20,
+    ok:"data",
+},
+{
+    id:2,
+    name: "shiva",
+    age: 20,
+    ok:"data",
+},
+{
+    id:3,
+    name: "shiva",
+    age: 20,
+    ok:"data",
+},
+{
+    id:4,
+    name: "shiva",
+    age: 20,
+    ok:"data",
+},
+{
+    id:5,
+    name: "shiva",
+    age: 20,
+    ok:"data",
+}
+]
---
+++ components/movies.js
@@ -0,0 +1,10 @@
+const movie1 = {
+    title: "Kalki",
+    time: "10AM",
+    seats: 60
+    };
+    const movie2 = {
+    title: "Pushpa 2",
+    time: "3PM",
+    seats: 60
+    };
